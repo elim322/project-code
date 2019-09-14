5cd89eb08a80e617282fac4a730c42b6f080e380
elim322 <einer.lim322@gmail.com>
2019-09-14 10:47:51 -0700

    add firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+let firebaseConfig = {
+  apiKey: 'AIzaSyA3wIvT-R0Ee-QhPy8uLBIH11eJSxks4qs',
+  authDomain: 'project-code-3a586.firebaseapp.com',
+  databaseURL: 'https://project-code-3a586.firebaseio.com',
+  projectId: 'project-code-3a586',
+  storageBucket: '',
+  messagingSenderId: '506642063107',
+  appId: '1:506642063107:web:394434aecf072237a45336',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
