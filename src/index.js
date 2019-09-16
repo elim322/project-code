ed3f9b332b7187d2f3a0485c43f0188e94480a45
elim322 <einer.lim322@gmail.com>
2019-09-16 12:47:37 -0700

    add config.json

--- src/index.js
+++ src/index.js
@@ -12,10 +12,11 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import 'tachyons';
 import './scss/styles.scss';
+import config from './config';
 
 // __SIMPLE_API_ENDPOINT__ looks like: 'https://api.graph.cool/simple/v1/__SERVICE_ID__'
 const httpLink = new HttpLink({
-  uri: 'https://api.graph.cool/simple/v1/ck0k2p72i0d750102p3hqsxyy',
+  uri: config.GRAPHQLAPI,
 });
 
 const client = new ApolloClient({
